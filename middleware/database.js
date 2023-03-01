971a552bc584dcdd77e4cd6e2f4c322691216d5d
Saim-Aqeel <64957047+Saim-Aqeel@users.noreply.github.com>
2023-03-02 00:36:56 +0500

    created db pool

--- middleware/database.js
+++ middleware/database.js
@@ -3,7 +3,7 @@ const config = require( '../config/db.config');
 
 class Database {
     constructor() {
-        this.connection = mysql.createConnection( config );
+        this.connection = mysql.createPool( config );
     }
     query( sql, args ) {
         return new Promise( ( resolve, reject ) => {
